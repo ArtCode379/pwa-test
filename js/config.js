0317bb996da0465b9d5d1c0927bc3613fc4047ae
ArtHouse379 <artemchechel1997@gmail.com>
2026-04-24 15:58:03 +0300

    config: set Railway proxy URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // После деплоя прокси на Railway замени на свой URL
 // Пример: 'https://spwinera-proxy.up.railway.app'
-const PROXY_URL = 'https://YOUR-PROXY.up.railway.app'
+const PROXY_URL = 'https://pwa-test-production-cd59.up.railway.app'
