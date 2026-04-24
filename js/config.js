ad54000d49c16b0b9b99d0c32bf5b8a7e65a2d3e
ArtHouse379 <artemchechel1997@gmail.com>
2026-04-24 16:52:14 +0300

    config: revert to Railway proxy URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // После деплоя прокси на Railway замени на свой URL
 // Пример: 'https://spwinera-proxy.up.railway.app'
-const PROXY_URL = 'https://unheated-impish-gangway.ngrok-free.dev'
+const PROXY_URL = 'https://pwa-test-production-cd59.up.railway.app'
